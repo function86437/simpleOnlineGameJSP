7572c1b737853e4832b69d780d28cb764633e59b
TangSong <jim20259@gmail.com>
2014-11-26 14:14:01 +0800

    Modified change password

--- src/server/DBconfig.php
+++ src/server/DBconfig.php
@@ -4,7 +4,7 @@
  */
 
 define('DB_USER', "root"); // db user
-define('DB_PASSWORD', "estimate74547"); // db password (mention your db password here)
+define('DB_PASSWORD', "1234asdf"); // db password (mention your db password here)
 define('DB_DATABASE', "simple_online_game"); // database name
 define('DB_SERVER', "localhost"); // db server
 ?>
