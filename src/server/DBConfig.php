07eb43e9059aa4571a6fdc7f521461106b991d94
function86437 <function86437@gmail.com>
2014-12-09 19:54:37 +0800

    PDO setting

---
+++ src/server/DBConfig.php
@@ -0,0 +1,13 @@
+<?php
+/*
+*   All database connection varisbles
+*   @version 1.0
+*/
+
+define("DB_USER","root");
+define("DB_PASSWD","estimate74547");
+define("DB_DATABASE","simple_online_game");
+define("DB_SERVER","localhost");
+define("DB_TYPE","mysql");
+define("DB_SERVER_CONFIG",DB_TYPE . ":host=" . DB_SERVER . ";dbname=" . DB_DATABASE . ";");
+?>
