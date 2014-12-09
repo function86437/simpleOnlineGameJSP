4f428b85eff4000ca1309fdcf1efc8047e536e49
xTangSongx <jim20259@gmail.com>
2014-12-09 19:58:51 +0800

    Modified index.html .js .css 製作email驗證功能（位完成）

--- src/web/asset/js/index.js
+++ src/web/asset/js/index.js
@@ -1,9 +1,22 @@
 /*jslint browser: true*/
-var signup;
+
+//DOM宣告
+var signup,account,password,checkPassword,email,nickname,birthday,gender;
 window.onload = function(){
     signup = document.getElementById("signup");
+
+    email=document.getElementsByName("email")[0];
+    email.addEventListener("keypress",verifyEmail);
 };
 
+
+
+
+//帳號畫面顯示
 function myFunction() {
     signup.style.visibility = "visible";
+}
+//驗證email正確性
+function verifyEmail(){
+    console.log("asdf");
 }
