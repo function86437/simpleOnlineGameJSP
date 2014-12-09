71c43f08a2851fbcae6555baf62793bf36d7b071
xTangSongx <jim20259@gmail.com>
2014-12-09 22:06:00 +0800

    Modified 導入AJAX驗證是否存在帳號

--- src/web/asset/js/index.js
+++ src/web/asset/js/index.js
@@ -1,11 +1,16 @@
 /*jslint browser: true*/
 
 //DOM宣告
-var signup,account,password,checkPassword,email,nickname,birthday,gender;
+var signup,loginAccount,loginPwd,signUpAccound,signUpPwd,signUpCheckPwd,email,nickname,birthday,gender;
+var virifyAccount = {
+    account:''
+};
 window.onload = function(){
     signup = document.getElementById("signup");
+    signUpAccount=document.getElementsByName("account")[1];
     email=document.getElementsByName("email")[0];
     email.addEventListener("keyup",verifyEmail);
+    signUpAccount.addEventListener("change",verifySignUpAccount);
 };
 
 
@@ -13,6 +18,19 @@ window.onload = function(){
 //帳號畫面顯示
 function myFunction() {
     signup.style.visibility = "visible";
+}
+
+function verifySignUpAccount(){
+    console.log(signUpAccount.value);
+    virifyAccount.account=signUpAccount.value;
+    ajaxImple("../server/duplicateAccount.php","POST",virifyAccount,function (response){
+        console.log(response);
+        if(response===true){
+            signUpAccount.className="valid";
+        }else{
+            signUpAccount.className="invalid";
+        }
+    });
 }
 //驗證輸入資料正確性
 function verifyEmail(){
