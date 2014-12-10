355e3950e6f7bc8bd4569c78e69de4538cf9a3f9
function86437 <function86437@gmail.com>
2014-12-10 12:22:30 +0800

    logout

--- src/server/logout.php
+++ src/server/logout.php
@@ -12,6 +12,26 @@ if(!isset($_SESSION['login'])){
     //redirect to index.html
     header("Location:../web/index.html");
 } else{
+    echo $_SESSION['nickname'];
+    $id = $_SESSION['id'];
+
+    require_once __DIR__ . '/DBConnect.php';
+
+    //new PDO connection
+    $dbcon = new DBConnect();
+
+    $sql = "DELETE FROM onlineuser WHERE account_id=?";
+
+    $result = $dbcon->query($sql,array($id));
+
+    if($result != null || $result != false) {
+
+        echo "true";
+        //DB connection close
+        $dbcon->close();
+    } else {
+        echo "false";
+    }
 
     session_destroy();
 
