083e80ed070395554a55235dd54aa861aa40101b
function86437 <function86437@gmail.com>
2014-12-10 10:46:11 +0800

    update user list

--- src/web/asset/js/user_list.js
+++ src/web/asset/js/user_list.js
@@ -3,24 +3,37 @@
 *   @version 1.0
 */
 (function refreshUserList() {
+
     //do something every three seconds
-    //setInterval(function(){
+    setInterval(function(){
         var object;
         ajaxImple("../server/showUserList.php","post",object,callback);
 
 
-    //},3000);
+    },3000);
 })();
 
-
+/*
+*   callback function
+*   @version 1.0
+*   @param response
+*/
 function callback(response) {
+    var userList = document.getElementById('user-list');
     var scroll = document.getElementById('scroll');
+
+    //if already exists,then remove
+    if(scroll != null) {
+        userList.removeChild(scroll);
+    }
+
     var users = JSON.parse(response);
     for(var i = 0 ; i < users.length ; i ++){
         console.log(users);
         var node = document.createElement('p');
-        node.setAttribute("id",i);
+        node.setAttribute("id","user"+i);
         node.innerHTML = users[i]['account'];
         scroll.appendChild(node);
     }
+    userList.appendChild(scroll);
 }
